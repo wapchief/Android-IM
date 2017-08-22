79f14d7ae5a7c1d1a26503243d01e67b1e45602c
apple <wapchief@qq.com>
2017-08-22 16:43:25 +0800

    拍照上传头像7.0适配

--- app/src/main/java/com/wapchief/jpushim/activity/EditUserInfoActivity.java
+++ app/src/main/java/com/wapchief/jpushim/activity/EditUserInfoActivity.java
@@ -2,15 +2,20 @@ package com.wapchief.jpushim.activity;
 
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.ClipData;
+import android.content.ContentValues;
 import android.content.Intent;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.BuildConfig;
+import android.support.v4.content.FileProvider;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
@@ -28,6 +33,7 @@ import com.wapchief.jpushim.R;
 import com.wapchief.jpushim.entity.RegionBean;
 import com.wapchief.jpushim.framework.base.BaseActivity;
 import com.wapchief.jpushim.framework.helper.SharedPrefHelper;
+import com.wapchief.jpushim.framework.utils.PhotoUtils;
 import com.wapchief.jpushim.framework.utils.StringUtils;
 import com.wapchief.jpushim.framework.utils.TimeUtils;
 import com.wapchief.jpushim.greendao.model.User;
@@ -93,6 +99,8 @@ public class EditUserInfoActivity extends BaseActivity {
     private static final int PHOTO_TK = 0;
     private static final int PHOTO_PZ = 1;
     private static final int PHOTO_CLIP = 2;
+    private String mFilePath;
+    Uri contentUri;
 
     @Override
     protected int setContentView() {
@@ -103,6 +111,8 @@ public class EditUserInfoActivity extends BaseActivity {
     protected void initView() {
         helper = SharedPrefHelper.getInstance();
         userInfo = JMessageClient.getMyInfo();
+        mFilePath = Environment.getExternalStorageDirectory().getPath();// 获取SD卡路径
+        mFilePath = mFilePath + "/" + "temp.jpg";// 指定路径
         initBar();
         initData();
         initGson();
@@ -118,11 +128,11 @@ public class EditUserInfoActivity extends BaseActivity {
 
     @Override
     protected void initData() {
-        mEditUserNackName.setText(userInfo.getNickname()+"");
-        mEditUserBirthday.setText(TimeUtils.ms2date("yyyy-MM-dd",userInfo.getBirthday()));
+        mEditUserNackName.setText(userInfo.getNickname() + "");
+        mEditUserBirthday.setText(TimeUtils.ms2date("yyyy-MM-dd", userInfo.getBirthday()));
         mEditUserGender.setText(StringUtils.constant2String(userInfo.getGender().name()));
-        mEditUserSignature.setText(userInfo.getSignature()+"");
-        mEditUserAddress.setText(userInfo.getAddress()+"");
+        mEditUserSignature.setText(userInfo.getSignature() + "");
+        mEditUserAddress.setText(userInfo.getAddress() + "");
 
     }
 
@@ -152,19 +162,19 @@ public class EditUserInfoActivity extends BaseActivity {
                 initRegionPickView();
                 break;
             case R.id.bottom_bar_tv2:
-                userInfo.setAddress(mEditUserAddress.getText()+"");
+                userInfo.setAddress(mEditUserAddress.getText() + "");
                 userInfo.setGender(UserInfo.Gender.valueOf(StringUtils.string2contant(mEditUserGender.getText().toString())));
-                userInfo.setBirthday(TimeUtils.date2ms("yyyy-MM-dd",mEditUserBirthday.getText().toString()));
-                userInfo.setSignature(mEditUserSignature.getText()+"");
-                userInfo.setNickname(mEditUserNackName.getText()+"");
+                userInfo.setBirthday(TimeUtils.date2ms("yyyy-MM-dd", mEditUserBirthday.getText().toString()));
+                userInfo.setSignature(mEditUserSignature.getText() + "");
+                userInfo.setNickname(mEditUserNackName.getText() + "");
                 JMessageClient.updateMyInfo(UserInfo.Field.all, userInfo, new BasicCallback() {
                     @Override
                     public void gotResult(int i, String s) {
-                        if (i==0){
-                            showToast(EditUserInfoActivity.this,"更新成功");
+                        if (i == 0) {
+                            showToast(EditUserInfoActivity.this, "更新成功");
                             finish();
-                        }else {
-                            showToast(EditUserInfoActivity.this,"更新失败："+s);
+                        } else {
+                            showToast(EditUserInfoActivity.this, "更新失败：" + s);
                         }
                     }
                 });
@@ -176,11 +186,16 @@ public class EditUserInfoActivity extends BaseActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         Log.e("result=========", requestCode + "\n" + resultCode + "\n" + data);
         if (resultCode == Activity.RESULT_OK) {
-            switch (requestCode){
+            switch (requestCode) {
                 case PHOTO_PZ:
-                    File pictur = new File(
-                            Environment.getExternalStorageDirectory() + "/temp.jpg");
-                    startPhotoZoom(Uri.fromFile(pictur));
+                    Uri pictur;
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//如果是7.0android系统
+                        pictur = contentUri;
+                    } else {
+                        pictur = Uri.fromFile(new File(
+                                Environment.getExternalStorageDirectory() + "/temp.jpg"));
+                    }
+                    startPhotoZoom(pictur);
                     break;
                 case PHOTO_TK:
                     startPhotoZoom(data.getData());
@@ -198,7 +213,7 @@ public class EditUserInfoActivity extends BaseActivity {
                         //创建新目录
                         file.mkdirs();
                         //以当前时间重新命名文件
-                        long i=System.currentTimeMillis();
+                        long i = System.currentTimeMillis();
                         //生成新的文件
                         file = new File(file.toString() + "/" + i + ".png");
                         Log.e("fileNew", file.getPath());
@@ -206,21 +221,21 @@ public class EditUserInfoActivity extends BaseActivity {
                         OutputStream out = new FileOutputStream(file.getPath());
                         //压缩文件
                         boolean flag = photo1.compress(Bitmap.CompressFormat.JPEG, 100, out);
-                        if (file.getName()!=null||!file.getName().equals("")){
+                        if (file.getName() != null || !file.getName().equals("")) {
                             showProgressDialog("正在上传头像");
-                                    JMessageClient.updateUserAvatar(file, new BasicCallback() {
-                                        @Override
-                                        public void gotResult(int i, String s) {
-                                            if (i==0){
-                                                dismissProgressDialog();
-                                                showToast(EditUserInfoActivity.this,"上传成功");
-                                            }else {
-                                                dismissProgressDialog();
-                                                showToast(EditUserInfoActivity.this,"上传失败："+s);
-                                            }
-                                        }
-                                    });
+                            JMessageClient.updateUserAvatar(file, new BasicCallback() {
+                                @Override
+                                public void gotResult(int i, String s) {
+                                    if (i == 0) {
+                                        dismissProgressDialog();
+                                        showToast(EditUserInfoActivity.this, "上传成功");
+                                    } else {
+                                        dismissProgressDialog();
+                                        showToast(EditUserInfoActivity.this, "上传失败：" + s);
+                                    }
                                 }
+                            });
+                        }
 
                     } catch (FileNotFoundException e) {
                         e.printStackTrace();
@@ -236,8 +251,11 @@ public class EditUserInfoActivity extends BaseActivity {
      * 头像裁剪
      */
     private Uri uritempFile;
+
     public void startPhotoZoom(Uri uri) {
+        Log.e("uri=====", "" + uri);
         Intent intent = new Intent("com.android.camera.action.CROP");
+
         intent.setDataAndType(uri, "image/*");
         intent.putExtra("crop", "true");
         intent.putExtra("aspectX", 1);
@@ -245,15 +263,25 @@ public class EditUserInfoActivity extends BaseActivity {
         intent.putExtra("outputX", 60);
         intent.putExtra("outputY", 60);
         //uritempFile为Uri类变量，实例化uritempFile
-        uritempFile = Uri.parse("file://" + "/" + Environment.getExternalStorageDirectory().getPath() + "/" + "small.jpg");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //开启临时权限
+            intent.addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION | Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            //重点:针对7.0以上的操作
+            intent.setClipData(ClipData.newRawUri(MediaStore.EXTRA_OUTPUT, uri));
+            uritempFile = uri;
+        } else {
+            uritempFile = Uri.parse("file://" + "/" + Environment.getExternalStorageDirectory().getPath() + "/" + "small.jpg");
+        }
         intent.putExtra(MediaStore.EXTRA_OUTPUT, uritempFile);
+        intent.putExtra("return-data", false);
         intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
         startActivityForResult(intent, PHOTO_CLIP);
     }
 
     /*头像选择器*/
     private void showHeadDialog() {
-        TextView update_dialog_TK,update_dialog_PZ,update_dialog_cancel;
+        TextView update_dialog_TK, update_dialog_PZ, update_dialog_cancel;
         View view = getLayoutInflater().inflate(R.layout.dialog_photo_type, null);
         dialog = new Dialog(this, R.style.dialog_animal);
 
@@ -262,22 +290,20 @@ public class EditUserInfoActivity extends BaseActivity {
                 display.getWidth()
                 , display.getHeight()
         ));
-//        dialog.getWindow().setWindowAnimations(R.style.Dialog_Anim_Style);
-//        dialog.setCanceledOnTouchOutside(true);
         //图库
-        update_dialog_TK=(TextView)view.findViewById(R.id.update_dialog_TK);
+        update_dialog_TK = (TextView) view.findViewById(R.id.update_dialog_TK);
         update_dialog_TK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK, null);
                 intent.setDataAndType(
                         MediaStore.Images.Media.INTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent,PHOTO_TK);
+                startActivityForResult(intent, PHOTO_TK);
                 dialog.dismiss();
             }
         });
         //取消
-        update_dialog_cancel= (TextView)view.findViewById(R.id.update_dialog_cancel);
+        update_dialog_cancel = (TextView) view.findViewById(R.id.update_dialog_cancel);
         update_dialog_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -290,15 +316,28 @@ public class EditUserInfoActivity extends BaseActivity {
         update_dialog_PZ.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                i.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(new File(
-                        Environment.getExternalStorageDirectory(), "temp.jpg")));
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);// 启动系统相机
+                Uri mImageCaptureUri;
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {//如果是7.0android系统
+                    intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                    contentUri = FileProvider.getUriForFile(EditUserInfoActivity.this,
+                            "com.wapchief.jpushim.fileProvider",
+                            new File(Environment.getExternalStorageDirectory(), "temp.jpg"));
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+                    Log.e("uriBC=====", "" + contentUri);
 
-                startActivityForResult(i, PHOTO_PZ);
+                } else {
+                    mImageCaptureUri = Uri.fromFile(new File(Environment.getExternalStorageDirectory(), "temp.jpg"));
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, mImageCaptureUri);
+                    Log.e("uriBC23=====", "" + mImageCaptureUri);
+
+                }
+                startActivityForResult(intent, PHOTO_PZ);
                 dialog.dismiss();
             }
         });
     }
+
     /*性别选择器*/
     private void initPickViewGender() {
         final ArrayList<String> options = new ArrayList<>();
@@ -314,8 +353,9 @@ public class EditUserInfoActivity extends BaseActivity {
         mOptionsPickerView.setPicker(options);
         mOptionsPickerView.show();
     }
+
     /*日期选择期*/
-    public void initTimePickView(){
+    public void initTimePickView() {
         TimePickerView pvTime = new TimePickerView.Builder(this, new TimePickerView.OnTimeSelectListener() {
             @Override
             public void onTimeSelect(Date date, View v) {//选中事件回调
@@ -332,35 +372,36 @@ public class EditUserInfoActivity extends BaseActivity {
         return format.format(date);
     }
 
-   /*地区选择器*/
-   protected void initRegionPickView(){
-       OptionsPickerView  pvOptions = new OptionsPickerView.Builder(this, new OptionsPickerView.OnOptionsSelectListener() {
-           @Override
-           public void onOptionsSelect(int options1, int options2, int options3, View v) {
-               //返回的分别是三个级别的选中位置
-               String tx = options1Items.get(options1).getPickerViewText()+
-                       options2Items.get(options1).get(options2)+
-                       options3Items.get(options1).get(options2).get(options3);
-               mEditUserAddress.setText(tx);
+    /*地区选择器*/
+    protected void initRegionPickView() {
+        OptionsPickerView pvOptions = new OptionsPickerView.Builder(this, new OptionsPickerView.OnOptionsSelectListener() {
+            @Override
+            public void onOptionsSelect(int options1, int options2, int options3, View v) {
+                //返回的分别是三个级别的选中位置
+                String tx = options1Items.get(options1).getPickerViewText() +
+                        options2Items.get(options1).get(options2) +
+                        options3Items.get(options1).get(options2).get(options3);
+                mEditUserAddress.setText(tx);
 
-           }
-       })
+            }
+        })
 
-               .setTitleText("城市选择")
-               .setDividerColor(Color.BLACK)
-               .setTextColorCenter(Color.BLACK) //设置选中项文字颜色
-               .setContentTextSize(20)
-               .setOutSideCancelable(false)// default is true
-               .build();
+                .setTitleText("城市选择")
+                .setDividerColor(Color.BLACK)
+                .setTextColorCenter(Color.BLACK) //设置选中项文字颜色
+                .setContentTextSize(20)
+                .setOutSideCancelable(false)// default is true
+                .build();
 
         /*pvOptions.setPicker(options1Items);//一级选择器
         pvOptions.setPicker(options1Items, options2Items);//二级选择器*/
-       pvOptions.setPicker(options1Items, options2Items,options3Items);//三级选择器
-       pvOptions.show();
-   }
+        pvOptions.setPicker(options1Items, options2Items, options3Items);//三级选择器
+        pvOptions.show();
+    }
+
     //解析地区
     private void initGson() {
-       //将json转化为String
+        //将json转化为String
         StringBuilder stringBuilder = new StringBuilder();
         try {
             AssetManager assetManager = this.getAssets();
@@ -378,11 +419,11 @@ public class EditUserInfoActivity extends BaseActivity {
 
         options1Items = jsonBean;
 
-        for (int i=0;i<jsonBean.size();i++){//遍历省份
+        for (int i = 0; i < jsonBean.size(); i++) {//遍历省份
             ArrayList<String> CityList = new ArrayList<>();//该省的城市列表（第二级）
             ArrayList<ArrayList<String>> Province_AreaList = new ArrayList<>();//该省的所有地区列表（第三极）
 
-            for (int c=0; c<jsonBean.get(i).getCityList().size(); c++){//遍历该省份的所有城市
+            for (int c = 0; c < jsonBean.get(i).getCityList().size(); c++) {//遍历该省份的所有城市
                 String CityName = jsonBean.get(i).getCityList().get(c).getName();
                 CityList.add(CityName);//添加城市
 
@@ -390,11 +431,11 @@ public class EditUserInfoActivity extends BaseActivity {
 
                 //如果无地区数据，建议添加空字符串，防止数据为null 导致三个选项长度不匹配造成崩溃
                 if (jsonBean.get(i).getCityList().get(c).getArea() == null
-                        ||jsonBean.get(i).getCityList().get(c).getArea().size()==0) {
+                        || jsonBean.get(i).getCityList().get(c).getArea().size() == 0) {
                     City_AreaList.add("");
-                }else {
+                } else {
 
-                    for (int d=0; d < jsonBean.get(i).getCityList().get(c).getArea().size(); d++) {//该城市对应地区所有数据
+                    for (int d = 0; d < jsonBean.get(i).getCityList().get(c).getArea().size(); d++) {//该城市对应地区所有数据
                         String AreaName = jsonBean.get(i).getCityList().get(c).getArea().get(d);
 
                         City_AreaList.add(AreaName);//添加该城市所有地区数据
---
+++ app/src/main/java/com/wapchief/jpushim/framework/utils/PhotoUtils.java
@@ -0,0 +1,233 @@
+package com.wapchief.jpushim.framework.utils;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ContentUris;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
+
+import java.io.File;
+
+/**
+ * Created by wapchief on 2017/8/22.
+ */
+
+public class PhotoUtils {
+    private static final String TAG = "PhotoUtils";
+
+    /**
+     * @param activity    当前activity
+     * @param imageUri    拍照后照片存储路径
+     * @param requestCode 调用系统相机请求码
+     */
+    public static void takePicture(Activity activity, Uri imageUri, int requestCode) {
+        //调用系统相机
+        Intent intentCamera = new Intent();
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intentCamera.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION); //添加这一句表示对目标应用临时授权该Uri所代表的文件
+        }
+        intentCamera.setAction(MediaStore.ACTION_IMAGE_CAPTURE);
+        //将拍照结果保存至photo_file的Uri中，不保留在相册中
+        intentCamera.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        activity.startActivityForResult(intentCamera, requestCode);
+    }
+
+    /**
+     * @param activity    当前activity
+     * @param requestCode 打开相册的请求码
+     */
+    public static void openPic(Activity activity, int requestCode) {
+        Intent photoPickerIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        photoPickerIntent.setType("image/*");
+        activity.startActivityForResult(photoPickerIntent, requestCode);
+    }
+
+    /**
+     * @param activity    当前activity
+     * @param orgUri      剪裁原图的Uri
+     * @param desUri      剪裁后的图片的Uri
+     * @param aspectX     X方向的比例
+     * @param aspectY     Y方向的比例
+     * @param width       剪裁图片的宽度
+     * @param height      剪裁图片高度
+     * @param requestCode 剪裁图片的请求码
+     */
+    public static void cropImageUri(Activity activity, Uri orgUri, Uri desUri, int aspectX, int aspectY, int width, int height, int requestCode) {
+        Intent intent = new Intent("com.android.camera.action.CROP");
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        }
+        intent.setDataAndType(orgUri, "image/*");
+        intent.putExtra("crop", "true");
+        intent.putExtra("aspectX", aspectX);
+        intent.putExtra("aspectY", aspectY);
+        intent.putExtra("outputX", width);
+        intent.putExtra("outputY", height);
+        intent.putExtra("scale", true);
+        //将剪切的图片保存到目标Uri中
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, desUri);
+        intent.putExtra("return-data", false);
+        intent.putExtra("outputFormat", Bitmap.CompressFormat.JPEG.toString());
+        intent.putExtra("noFaceDetection", true);
+        activity.startActivityForResult(intent, requestCode);
+    }
+
+    /**
+     * 读取uri所在的图片
+     *
+     * @param uri      图片对应的Uri
+     * @param mContext 上下文对象
+     * @return 获取图像的Bitmap
+     */
+    public static Bitmap getBitmapFromUri(Uri uri, Context mContext) {
+        try {
+            Bitmap bitmap = MediaStore.Images.Media.getBitmap(mContext.getContentResolver(), uri);
+            return bitmap;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    /**
+     * @param context 上下文对象
+     * @param uri     当前相册照片的Uri
+     * @return 解析后的Uri对应的String
+     */
+    @SuppressLint("NewApi")
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+        String pathHead = "file:///";
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+                if ("primary".equalsIgnoreCase(type)) {
+                    return pathHead + Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+
+                final Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return pathHead + getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{split[1]};
+
+                return pathHead + getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return pathHead + getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return pathHead + uri.getPath();
+        }
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    private static String getDataColumn(Context context, Uri uri, String selection, String[] selectionArgs) {
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {column};
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    private static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    private static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+    //路径 ontext.getCacheDir()
+    public static String pathForTempPhoto(Context context, String fileName) {
+        final File dir = context.getCacheDir();
+        dir.mkdirs();
+        final File f = new File(dir, fileName);
+        return f.getAbsolutePath();
+    }
+
+    /**
+     * FileProvider转uri
+     * @param context
+     * @param fileName
+     * @return
+     */
+    public static Uri getOutUri(Context context,String fileName){
+        final String  fileProviderAuthority ="com.wapchief.jpushim.fileProvider" ;
+        File ff = new File(pathForTempPhoto(context, fileName));
+        Uri uri = FileProvider.getUriForFile(context, fileProviderAuthority,ff);
+        return  uri;
+    }
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    private static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
+    }
+
+}
